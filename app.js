1f86f0ffc0ce9fe29b8c4d33b0cbe5358a3ffe51
julian <julisanchez1991@gmail.com>
2019-11-17 14:19:19 -0300

    Subida a github

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.getElementById("formtask").addEventListener("submit",savetask);
 
 function savetask(e){
     let title = document.getElementById("title").value;
-    let description = document.getElementById("description").valueM
+    let description = document.getElementById("description").value;
 
     const task = {
         title,
@@ -18,6 +18,32 @@ function savetask(e){
         tasks.push(task);
         localStorage.setItem("tasks",JSON.stringify(tasks));
     }
-
+    getTask();
     e.preventDefault();
+}
+
+function getTask(){
+   let tasks =  JSON.parse(localStorage.getItem("tasks"));
+   let tasksView = document.getElementById("tasks");
+
+   tasksView.innerHTML = '';
+
+   for(let i = 0; i<tasks.length;i++){
+        let title = tasks[i].title;
+        let description = tasks[i].description;
+        tasksView.innerHTML +=`<div class="card mb-3"><div class="card-body"><p>${title} - ${description}</p><a class="btn btn-danger" onclick="deleteTask('${title}')">Delete</a></div></div>`;
+   }
+}
+
+getTask();
+
+function deleteTask(title){
+    let tasks= JSON.parse(localStorage.getItem("tasks"));
+    for(let i = 0;i<tasks.length;i++){
+        if(tasks[i].title == title){
+            tasks.splice(i,1);
+        }
+    }
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+    getTask();
 }
